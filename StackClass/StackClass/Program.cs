dd40da81342a76441bc7459708799916ac362e37
BalotaGeorge <56297936+BalotaGeorge@users.noreply.github.com>
2020-05-17 14:44:15 +0300

    Add files via upload

---
+++ StackClass/StackClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> ints = new Stack<int>(3);
+            ints.Push(4);
+            Console.WriteLine(ints.Pop());
+            ints.Pop();
+            ints.Push(4);
+            ints.Push(4);
+            ints.Push(4);
+            ints.Push(4);
+
+            Console.WriteLine();
+
+            Stack<string> strings = new Stack<string>(2);
+            strings.Push("Hey");
+            strings.Push("Ya");
+            strings.Clear();
+            strings.Push("!");
+            Console.WriteLine(strings.Pop());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackClass/StackClass/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackClass
+{
+    public class Stack<T>
+    {
+        public T[] items;
+        private int index;
+        public int Count { get { return index >= 0 ? index + 1 : 0; } set { index = value; } }
+
+        public Stack(int cantity)
+        {
+            index = -1;
+            items = new T[cantity];
+        }
+
+        public void Push(T item)
+        {
+            if (index + 1 >= items.Length)
+            {
+                Console.WriteLine("The Stack is full, can't add anymore items");
+            }
+            else
+            {
+                index++;
+                items[index] = item;
+            }
+        }
+
+        public T Pop()
+        {
+            if (index < 0)
+            {
+                Console.WriteLine("There are no more items to pop out of the Stack");
+                return default;
+            }
+            T value = items[index];
+            items[index] = default;
+            index--;
+            return value;
+        }
+
+        public void Clear()
+        {
+            index = 0;
+            items = new T[items.Length];
+        }
+    }
+}
