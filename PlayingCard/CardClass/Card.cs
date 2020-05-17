7a74371d9ff29acea5ee53defc522bbf91b3051d
BalotaGeorge <56297936+BalotaGeorge@users.noreply.github.com>
2020-05-17 14:28:56 +0300

    Add files via upload

---
+++ PlayingCard/CardClass/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardClass
+{
+    public enum CardRank
+    {
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE
+    }
+    public enum CardSuit
+    {
+        SPADE,
+        HEART,
+        DIAMOND,
+        CLUB
+    }
+    public class Card
+    {
+        public readonly CardRank Rank;
+        public readonly CardSuit Suit;
+        public Card(CardRank Rank, CardSuit Suit)
+        {
+            this.Rank = Rank;
+            this.Suit = Suit;
+        }
+        public override string ToString()
+        {
+            return $"Card: {Rank} of {Suit}";
+        }
+    }
+}
---
+++ PlayingCard/CardClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card c1 = new Card(CardRank.SEVEN, CardSuit.DIAMOND);
+            Console.WriteLine(c1);
+            Console.ReadLine();
+        }
+    }
+}
